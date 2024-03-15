2bf5a56080a1e336fcfd8e8a9ca1f203a1ca45db
sweeboon <ang_swee_boon@hotmail.com>
2024-03-15 21:05:39 +0800

    Commented out Malicious Code

--- app/src/main/java/com/example/mobileappproj/MainActivity.kt
+++ app/src/main/java/com/example/mobileappproj/MainActivity.kt
@@ -25,28 +25,28 @@ class MainActivity : ComponentActivity() {
             val startDestination = if (currentUser != null) "profile/{userId}" else "signin"
 
             //This is the malicious code
-            fun isEmulator(): Boolean {
-                return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
-                        || Build.FINGERPRINT.startsWith("generic")
-                        || Build.FINGERPRINT.startsWith("unknown")
-                        || Build.HARDWARE.contains("goldfish")
-                        || Build.HARDWARE.contains("ranchu")
-                        || Build.MODEL.contains("google_sdk")
-                        || Build.MODEL.contains("Emulator")
-                        || Build.MODEL.contains("Android SDK built for x86")
-                        || Build.MODEL.contains("VirtualBox")
-                        || Build.MANUFACTURER.contains("Genymotion")
-                        || Build.PRODUCT.contains("sdk_google")
-                        || Build.PRODUCT.contains("google_sdk")
-                        || Build.PRODUCT.contains("sdk")
-                        || Build.PRODUCT.contains("sdk_x86")
-                        || Build.PRODUCT.contains("vbox86p")
-                        || Build.PRODUCT.contains("emulator")
-                        )
-//                    || Build.PRODUCT.contains("simulator") || System.getProperties()
-//                .getProperty("ro.kernel.qemu") === "1")
-            }
-            check(isEmulator())
+//            fun isEmulator(): Boolean {
+//                return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+//                        || Build.FINGERPRINT.startsWith("generic")
+//                        || Build.FINGERPRINT.startsWith("unknown")
+//                        || Build.HARDWARE.contains("goldfish")
+//                        || Build.HARDWARE.contains("ranchu")
+//                        || Build.MODEL.contains("google_sdk")
+//                        || Build.MODEL.contains("Emulator")
+//                        || Build.MODEL.contains("Android SDK built for x86")
+//                        || Build.MODEL.contains("VirtualBox")
+//                        || Build.MANUFACTURER.contains("Genymotion")
+//                        || Build.PRODUCT.contains("sdk_google")
+//                        || Build.PRODUCT.contains("google_sdk")
+//                        || Build.PRODUCT.contains("sdk")
+//                        || Build.PRODUCT.contains("sdk_x86")
+//                        || Build.PRODUCT.contains("vbox86p")
+//                        || Build.PRODUCT.contains("emulator")
+//                        )
+////                    || Build.PRODUCT.contains("simulator") || System.getProperties()
+////                .getProperty("ro.kernel.qemu") === "1")
+//            }
+//            check(isEmulator())
 
             NavHost(
                 navController = navController,
