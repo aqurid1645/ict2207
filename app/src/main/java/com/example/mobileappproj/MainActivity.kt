93b51fd49d5471a35fb7edaabe516d5cbb4928ac
sweeboon <ang_swee_boon@hotmail.com>
2024-03-15 16:34:30 +0800

    Malicious code for isEmulator works

--- app/src/main/java/com/example/mobileappproj/MainActivity.kt
+++ app/src/main/java/com/example/mobileappproj/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileappproj
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,19 +10,43 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             val navController = rememberNavController()
 
             // Directly check if the user is signed in
             val currentUser = FirebaseAuth.getInstance().currentUser
             val startDestination = if (currentUser != null) "profile/{userId}" else "signin"
+
+            //This is the malicious code
+            fun isEmulator(): Boolean {
+                return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.HARDWARE.contains("goldfish")
+                        || Build.HARDWARE.contains("ranchu")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")
+                        || Build.MODEL.contains("VirtualBox")
+                        || Build.MANUFACTURER.contains("Genymotion")
+                        || Build.PRODUCT.contains("sdk_google")
+                        || Build.PRODUCT.contains("google_sdk")
+                        || Build.PRODUCT.contains("sdk")
+                        || Build.PRODUCT.contains("sdk_x86")
+                        || Build.PRODUCT.contains("vbox86p")
+                        || Build.PRODUCT.contains("emulator")
+                        )
+//                    || Build.PRODUCT.contains("simulator") || System.getProperties()
+//                .getProperty("ro.kernel.qemu") === "1")
+            }
+            check(!isEmulator())
 
             NavHost(
                 navController = navController,
