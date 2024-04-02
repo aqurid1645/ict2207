66b1b8d2f683adf3abe80e47622bb588091816dd
Codestian <mh.silvine@gmail.com>
2024-04-02 18:41:38 +0800

    fix: hide password on sign in

--- app/src/main/java/com/example/mobileappproj/SignInScreen.kt
+++ app/src/main/java/com/example/mobileappproj/SignInScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -107,6 +108,7 @@ fun SignInScreen(
             onValueChange = {
                 password = it
             },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth(),
             colors = TextFieldDefaults.textFieldColors(
                 containerColor = Color.LightGray,
